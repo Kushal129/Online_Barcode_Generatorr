d9a715a5722ec0f04339b347a4c5ad90df5b6615
kushal129 <21bmiit129@gmail.com>
2023-12-23 18:02:16 +0530

    init

--- index.php
+++ index.php
@@ -20,89 +20,110 @@ if (isset($_POST['clear_data'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Barcode Generator</title>
+    <link rel="icon" type="image/x-icon" href="urllogo.jpg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <div class="container">
-        <h1 class="mt-5 mb-4">Barcode Generator</h1>
-        <hr>
-        <form method="post">
-            <div class="mb-3">
-                <label for="barcode_data" class="form-label">Enter Barcode Data (one per line):</label>
-                <textarea class="form-control" name="barcode_data" rows="10" cols="30"><?php echo isset($_SESSION['barcode_data']) ? htmlspecialchars($_SESSION['barcode_data']) : ''; ?></textarea>
-            </div>
+    <div class="area">
 
-            <button type="submit" class="btn btn-primary" name="submit">Generate Barcodes</button>
-            <button type="submit" class="btn btn-danger" name="clear_data">Clear Data</button>
-        </form>
-
-        <?php
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $_SESSION['barcode_data'] = $_POST['barcode_data'];
-
-            if (empty(trim($_SESSION['barcode_data']))) {
-                ?>
-                <div class="alert alert-danger mt-3" role="alert">
-                    No barcode data found. Please enter barcode data and try again.
+        <div class="container">
+            <h1 class="mt-5 mb-4">Barcode Generator</h1>
+            <hr>
+            <form method="post">
+                <div class="mb-3">
+                    <label for="barcode_data" class="form-label">Enter Barcode Data (one per line):</label>
+                    <textarea class="form-control" name="barcode_data" rows="10" cols="30"><?php echo isset($_SESSION['barcode_data']) ? htmlspecialchars($_SESSION['barcode_data']) : ''; ?></textarea>
                 </div>
-                <?php
-                exit; 
-            }
-        }
-
-        if (isset($_SESSION['barcode_data'])) {
-            $barcodeData = $_SESSION['barcode_data'];
-            $barcodeArray = explode("\n", $barcodeData);
-            if (count($barcodeArray) > 300) {
-                ?>
-                <div class="alert alert-danger mt-3" role="alert">
-                    You can only generate a maximum of 300 barcodes at a time.
+                <div class="button-container">
+                    <button type="submit" class="btn " name="submit">Generate Barcodes</button>
+                    <button type="submit" class="btn " name="clear_data">Clear Data</button>
                 </div>
-                <?php
-                exit; 
-            }
-            $generator = new BarcodeGeneratorHTML();
-            $itemsPerPage = 30;
-            $totalItems = count($barcodeArray);
-            $totalPages = ceil($totalItems / $itemsPerPage);
-            $currentPage = isset($_GET['page']) ? max(1, min($totalPages, (int)$_GET['page'])) : 1;
-            $offset = ($currentPage - 1) * $itemsPerPage;
-            ?>
-            <div class="barcode-container">
-                <?php
-                for ($i = $offset; $i < min($offset + $itemsPerPage, $totalItems); $i++) {
-                    $data = $barcodeArray[$i];
-                    echo '<div class="barcode-box">';
-                    echo '<p><strong>Barcode Data:</strong> ' . htmlspecialchars($data) . '</p>';
-                    echo '<p><strong>Number:</strong> ' . ($i + 1) . '</p>';
-                    $barcodeHtml = $generator->getBarcode($data, $generator::TYPE_CODE_128);
-                    $barcodeHtml = str_replace('height:30px;', 'height:80px;', $barcodeHtml);
-                    echo $barcodeHtml;
-                    echo '</div>';
-                }
-                ?>
-            </div>
-            <nav class="pagination">
-                <ul class="pagination">
-                    <?php for ($page = 1; $page <= $totalPages; $page++) : ?>
-                        <li class="page-item <?php echo $page == $currentPage ? 'active' : ''; ?>">
-                            <a class="page-link" href="?page=<?php echo $page; ?>"><?php echo $page; ?></a>
-                        </li>
-                    <?php endfor; ?>
-                </ul>
-            </nav>
+            </form>
+
             <?php
-        }
-        ?>
-        <div class="signature">
-            <h6>- DJ Joker & Mr.KH</h6>
-        </div>
-    </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                $_SESSION['barcode_data'] = $_POST['barcode_data'];
+
+                if (empty(trim($_SESSION['barcode_data']))) {
+            ?>
+                    <div class="alert alert-danger mt-3" role="alert">
+                        No barcode data found. Please enter barcode data and try again.
+                    </div>
+                <?php
+                    exit;
+                }
+            }
+
+            if (isset($_SESSION['barcode_data'])) {
+                $barcodeData = $_SESSION['barcode_data'];
+                $barcodeArray = explode("\n", $barcodeData);
+                if (count($barcodeArray) > 300) {
+                ?>
+                    <div class="alert alert-danger mt-3" role="alert">
+                        You can only generate a maximum of 300 barcodes at a time.
+                    </div>
+                <?php
+                    exit;
+                }
+                $generator = new BarcodeGeneratorHTML();
+                $itemsPerPage = 30;
+                $totalItems = count($barcodeArray);
+                $totalPages = ceil($totalItems / $itemsPerPage);
+                $currentPage = isset($_GET['page']) ? max(1, min($totalPages, (int)$_GET['page'])) : 1;
+                $offset = ($currentPage - 1) * $itemsPerPage;
+                ?>
+                <div class="barcode-container">
+                    <?php
+                    for ($i = $offset; $i < min($offset + $itemsPerPage, $totalItems); $i++) {
+                        $data = $barcodeArray[$i];
+                        echo '<div class="barcode-box">';
+                        echo '<p><strong>Barcode Data:</strong> ' . htmlspecialchars($data) . '</p>';
+                        echo '<p><strong>Number:</strong> ' . ($i + 1) . '</p>';
+                        $barcodeHtml = $generator->getBarcode($data, $generator::TYPE_CODE_128);
+                        $barcodeHtml = str_replace('height:30px;', 'height:80px;', $barcodeHtml);
+                        echo $barcodeHtml;
+                        echo '</div>';
+                    }
+                    ?>
+                </div>
+                <nav class="pagination">
+                    <ul class="pagination">
+                        <?php for ($page = 1; $page <= $totalPages; $page++) : ?>
+                            <li class="page-item <?php echo $page == $currentPage ? 'active' : ''; ?>">
+                                <a class="page-link" href="?page=<?php echo $page; ?>"><?php echo $page; ?></a>
+                            </li>
+                        <?php endfor; ?>
+                    </ul>
+                </nav>
+            <?php
+            }
+            ?>
+            <div class="signature">
+                <h6>- DJ Joker & Mr.KH</h6>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
+
+        <ul class="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
 </body>
 
 </html>
+<!-- 
+Developed by Kushal Pipaliya 
+Github :- https://github.com/Kushal129   
+-->
